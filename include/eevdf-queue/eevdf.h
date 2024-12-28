4fc72ae9c0f6fe8c8d00ee50523d8f32ff2b2ba8
Noam Raz <noamraz8@gmail.com>
2024-12-28 15:11:01 +0200

    Start implementing redesigned scheduler
    This redesign fixes a few bugs and is more similar to the Linux
    implementation: it supports arbitrary weights instead of fixed
    priorities and avoids linear-time operations by not trying to shuffle
    nodes across several queues.
    
    This new implementation doesn't yet support removing nodes that have
    been added to the queue; that will come later.

--- include/eevdf-queue/eevdf.h
+++ include/eevdf-queue/eevdf.h
@@ -1,75 +1,37 @@
 #pragma once
 
+#include <linux/rbtree.h>
 #include <stdbool.h>
 #include <stdint.h>
-
-typedef enum eevdf_priority {
-    EEVDF_PRIORITY_LOWEST,
-    EEVDF_PRIORITY_BELOW_NORMAL,
-    EEVDF_PRIORITY_NORMAL,
-    EEVDF_PRIORITY_ABOVE_NORMAL,
-    EEVDF_PRIORITY_HIGHEST,
-    EEVDF_PRIORITY_MAX
-} eevdf_priority_t;
 
 typedef struct eevdf_queue eevdf_queue_t;
 typedef struct eevdf_node eevdf_node_t;
 
 typedef struct eevdf_node {
-    //
-    // Configurable by the scheduler
-    //
-
     // the priority of the node
-    eevdf_priority_t priority;
+    uint8_t weight;
 
     // the time slice the node needs/wants (physical time)
     uint32_t time_slice;
 
-    //
-    // Controlled by the eevdf queue
-    //
-
-    // the queue this node is on, NULL if not attached to any queue
     eevdf_queue_t* current_queue;
+    struct rb_node timeline_node;
 
-    // when attached to an eevdf queue, this is the base lag that was at
-    // the time of insertion to the queue
-    int64_t ideal_runtime_base;
-
-    // the physical runtime of the process, when not queued
-    // in any queue this will have the -lag so that the next
-    // lag calculations will return the same value as it had
-    // before
-    int64_t runtime;
-
-    // the virtual deadline of the node
-    uint64_t virtual_deadline;
-
-    // if true then the node is decaying while being parked
-    // once the lag becomes positive reset it
-    // if false and we are parked then it means that once
-    // the lag becomes positive we need to requeue it
-    bool remove;
+    int64_t vdeadline;
+    int64_t vruntime;
+    int64_t min_vruntime;
 } eevdf_node_t;
 
 typedef struct eevdf_queue {
-    // the accumulated ideal runtime for each of the weights
-    // in physical time
-    int64_t total_ideal_runtime[EEVDF_PRIORITY_MAX];
+    int64_t vtime;
+    uint32_t total_weight;
 
-    // the current virtual time of the queue
-    int64_t virtual_time;
-
-    // the total amount of weights we have in the queue
-    uint32_t weights_sum;
-
-    // the currently running node
+    struct rb_root_cached timeline;
     eevdf_node_t* current;
 } eevdf_queue_t;
 
 /**
- * Add a new thread into the queue
+ * Add a new thread into the queue.
  *
  * @param queue         [IN] The queue to add to
  * @param node          [IN] The thread that we are adding
@@ -77,22 +39,11 @@ typedef struct eevdf_queue {
 void eevdf_queue_add(eevdf_queue_t* queue, eevdf_node_t* node);
 
 /**
- * Wakeup a node, re-queueing it to the queue it was last on
- *
- * The only case where you want to wakeup is when the node that was
- * parked
- *
- * @param node          [IN] The node that we need to requeue
- */
-void eevdf_node_wakeup(eevdf_node_t* node);
-
-/**
- * Schedule a node to run, will properly handle the current, if the last tick returned NULL
- * then this function should not be called until another thread either wakesup or gets added
+ * Schedule a node to run, will properly handle the current, if the last tick
+ * returned NULL then this function should not be called until another thread
+ * either wakes up or gets added.
  *
  * @param queue         [IN] The queue we are scheduling on
  * @param time_slice    [IN] The time since the last time schedule was called
- * @param remove        [IN] Should the current be removed
- * @param requeue       [IN] Should the current be requeued
  */
-eevdf_node_t* eevdf_queue_schedule(eevdf_queue_t* queue, int64_t time_slice, bool remove, bool requeue);
+eevdf_node_t* eevdf_queue_schedule(eevdf_queue_t* queue, int64_t time_slice);
--- src/eevdf.c
+++ src/eevdf.c
@@ -1,15 +1,143 @@
 #include <eevdf-queue/eevdf.h>
-
+#include <linux/rbtree.h>
+#include <linux/rbtree_augmented.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
+
+#define EEVDF_NODE_OF(node) rb_entry(node, eevdf_node_t, timeline_node)
+#define EEVDF_NODE_OF_SAFE(node)                                               \
+    rb_entry_safe(node, eevdf_node_t, timeline_node)
+
+static inline void set_deadline(eevdf_node_t* node) {
+    // The node will always become eligible exactly when its vruntime matches
+    // the global vtime, so its eligible vtime is actually just its vruntime.
+    node->vdeadline = node->vruntime + node->time_slice / node->weight;
+}
+
+static inline bool is_eligible(eevdf_node_t* node, int64_t vtime) {
+    return node->vruntime <= vtime;
+}
+
+static inline bool subtree_has_eligible_node(eevdf_node_t* root,
+                                             int64_t vtime) {
+    return root->min_vruntime <= vtime;
+}
+
+static inline bool deadline_before(struct rb_node* lhs,
+                                   const struct rb_node* rhs) {
+    return EEVDF_NODE_OF(lhs)->vdeadline < EEVDF_NODE_OF(rhs)->vdeadline;
+}
+
+static bool update_min_vruntime(eevdf_node_t* node, bool exit) {
+    int64_t min_vruntime = node->vruntime;
+
+    if (node->timeline_node.rb_left) {
+        int64_t left_min_vruntime =
+            EEVDF_NODE_OF(node->timeline_node.rb_left)->min_vruntime;
+        if (left_min_vruntime < min_vruntime) {
+            min_vruntime = left_min_vruntime;
+        }
+    }
+
+    if (node->timeline_node.rb_right) {
+        int64_t right_min_vruntime =
+            EEVDF_NODE_OF(node->timeline_node.rb_right)->min_vruntime;
+        if (right_min_vruntime < min_vruntime) {
+            min_vruntime = right_min_vruntime;
+        }
+    }
+
+    bool done = min_vruntime == node->min_vruntime;
+    node->min_vruntime = min_vruntime;
+
+    return done;
+}
+
+RB_DECLARE_CALLBACKS(static, min_vruntime_callbacks, eevdf_node_t,
+                     timeline_node, min_vruntime, update_min_vruntime);
+
+static void enqueue_node(eevdf_queue_t* queue, eevdf_node_t* node) {
+    rb_add_augmented_cached(&node->timeline_node, &queue->timeline,
+                            deadline_before, &min_vruntime_callbacks);
+}
+
+static void dequeue_node(eevdf_queue_t* queue, eevdf_node_t* node) {
+    rb_erase_augmented_cached(&node->timeline_node, &queue->timeline,
+                              &min_vruntime_callbacks);
+}
+
+static eevdf_node_t* eevdf_queue_pick(eevdf_queue_t* queue) {
+    // Optimization: we have easy access to the node with the earliest
+    // deadline, and it will be the correct choice if it is eligible. Skip the
+    // tree walk in that case.
+    eevdf_node_t* earliest_deadline =
+        EEVDF_NODE_OF(rb_first_cached(&queue->timeline));
+    if (is_eligible(earliest_deadline, queue->vtime)) {
+        return earliest_deadline;
+    }
+
+    // Slow case: walk down the tree, searching for the leftmost eligible node.
+    // We can use the `min_vruntime` field to prune entire subtrees that are
+    // ineligible.
+
+    // Assumption: our tree always contains at least one eligible node (because
+    // lags always sum to 0). We can then derive the loop invariant that `node`
+    // always has at least one eligible node in its subtree.
+
+    eevdf_node_t* node = EEVDF_NODE_OF(queue->timeline.rb_root.rb_node);
+    while (true) {
+        eevdf_node_t* left = EEVDF_NODE_OF_SAFE(node->timeline_node.rb_left);
+
+        // If the node's left subtree has any eligible nodes, descend into it.
+        if (left && subtree_has_eligible_node(left, queue->vtime)) {
+            node = left;
+            continue;
+        }
+
+        // `node` doesn't have any eligible left descendents. If it is eligible
+        // itself, it is the leftmost eligible node.
+        if (is_eligible(node, queue->vtime)) {
+            break;
+        }
+
+        // Otherwise, all eligible nodes must reside in the right subtree (which
+        // must necessarily exist by the loop invariant). Descend there now.
+        node = EEVDF_NODE_OF(node->timeline_node.rb_right);
+    }
+
+    return node;
+}
 
 void eevdf_queue_add(eevdf_queue_t* queue, eevdf_node_t* node) {
+    queue->total_weight += node->weight;
 
+    // For now: always insert with a lag of 0.
+    node->vruntime = queue->vtime;
+    node->current_queue = queue;
+    set_deadline(node);
+    enqueue_node(queue, node);
 }
 
-void eevdf_node_wakeup(eevdf_node_t* node) {
+eevdf_node_t* eevdf_queue_schedule(eevdf_queue_t* queue, int64_t time_slice) {
+    if (!queue->total_weight) {
+        // If nothing is currently running on this queue, our virtual clock is
+        // paused and nothing can be selected for execution.
+        return NULL;
+    }
 
-}
+    queue->vtime += time_slice / queue->total_weight;
 
-eevdf_node_t* eevdf_queue_schedule(eevdf_queue_t* queue, int64_t time_slice, bool remove, bool requeue) {
-    return NULL;
+    eevdf_node_t* current = queue->current;
+    if (current) {
+        current->vruntime += time_slice / current->weight;
+        set_deadline(current);
+        enqueue_node(queue, current);
+    }
+
+    eevdf_node_t* next = eevdf_queue_pick(queue);
+    dequeue_node(queue, next);
+    queue->current = next;
+
+    return next;
 }
